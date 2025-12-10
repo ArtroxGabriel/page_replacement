de5cbf0cb9bf755b5f5e1bfb052f7f8bb4865604
erikbayerlein <erik.bayer@icloud.com>
2025-12-10 15:59:38 -0300

    refactor: updating Pager to work with TraceReader

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,15 @@ package org.example;
 import org.example.cli.Pager;
 import picocli.CommandLine;
 
-public class Main {
+// cmd example: ./gradlew run --args="--algo FIFO --frames 3 --trace silberschatz2001.trace --verbose"
 
-  // cmd example: ./gradlew run --args="--algo FIFO --frames 3 --trace src/main/resources/silberschatz2001.trace --verbose"
+public class Main {
   public static void main(String[] args) {
     if (args.length == 0) {
       args = new String[]{
           "--algo", "FIFO",
           "--frames", "3",
-          "--trace", "src/main/resources/silberschatz2001.trace",
+          "--trace", "silberschatz2001.trace",
           "--verbose"
       };
     }

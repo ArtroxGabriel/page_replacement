e6d0cdb39a4436f571fb8c4bdfea7300687fa793
Gabrigas <artroxgabriel@gmail.com>
2025-12-12 16:32:57 -0300

    feat: add log4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 
 dependencies {
     implementation("info.picocli:picocli:4.6.3")
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
